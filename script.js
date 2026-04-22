bfffc9bc516772e9718fbed87ea815d9a4095a89
CodeGemR <ruby.kodehode@gmail.com>
2026-04-22 08:59:09 +0200

    audio fix

--- script.js
+++ script.js
@@ -6,18 +6,21 @@ function spawnMeisho(){
         if(randomPic > 1){
             image.src = "./images/doto.webp"
             image.alt = "Meisho"
-            // const audio = new Audio("./audio/meishodoto.mp3")
-            // audio.play()
+            const audio = new Audio("./audio/meishodoto.mp3")
+            audio.volume = 0.1;
+            audio.play()
         }else if (randomPic === 1){
             image.src = "./images/meisho.png"
             image.alt = "Meisho"
             image.style.width = "250px";
             const audio = new Audio("./audio/meishoNeigh.mp3")
+            audio.volume = 0.1;
             audio.play()
         }else{
             image.src = "./images/gold.png"
             image.alt = "Goldship"
             const audio = new Audio("./audio/goldImpost.mp3")
+            audio.volume = 0.1;
             audio.play()
         }
     container.appendChild(image)
