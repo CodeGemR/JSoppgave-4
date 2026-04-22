1e6dd7445c187385912b108fa2f08a36521187ee
CodeGemR <ruby.kodehode@gmail.com>
2026-04-22 09:04:14 +0200

    fixed chances

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function spawnMeisho(){
     const container = document.getElementById("container");
     const image = document.createElement("img");
     const randomPic = Math.floor(Math.random() * 100);
-        if(randomPic > 1){
+        if(randomPic < 60){
             image.src = "./images/doto.webp"
             image.alt = "Meisho"
             const audio = new Audio("./audio/meishodoto.mp3")
             audio.volume = 0.1;
             audio.play()
-        }else if (randomPic === 1){
+        }else if (randomPic < 80){
             image.src = "./images/meisho.png"
             image.alt = "Meisho"
             image.style.width = "250px";
